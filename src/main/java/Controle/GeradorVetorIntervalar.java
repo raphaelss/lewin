adae9dd61395a59a703b8a845fd462b1dad9b9ec
Raphael Santos <contact@raphaelss.com>
2015-07-30 10:57:51 -0300

    Simplificação de algoritmo de vetor intervalar através do uso de ClasseDeAltura.intervalo_desord.

--- src/main/java/Controle/GeradorVetorIntervalar.java
+++ src/main/java/Controle/GeradorVetorIntervalar.java
@@ -25,42 +25,13 @@ import java.util.ArrayList;
 
 public class GeradorVetorIntervalar {
     public static int[] geraVetor(ArrayList<ClasseDeAltura> entrada) {
-        ArrayList<Integer> diferencas = new ArrayList<Integer>();
-
-        for (int i = 0; i < entrada.size(); i++) {
-            for (int j = i + 1; j < entrada.size(); j++) {
-                diferencas.add(Math.abs(entrada.get(i).diferenca(entrada.get(j))));
-            }
-        }
         int[] contadores = new int[6];
-
-        for (int numero : diferencas) {
-            switch(numero) {
-                case 1:
-                case 11:
-                    contadores[0]++;
-                    break;
-                case 2:
-                case 10:
-                    contadores[1]++;
-                    break;
-                case 3:
-                case 9:
-                    contadores[2]++;
-                    break;
-                case 4:
-                case 8:
-                    contadores[3]++;
-                    break;
-                case 5:
-                case 7:
-                    contadores[4]++;
-                    break;
-                case 6:
-                    contadores[5]++;
+        int tamanho = entrada.size();
+        for (int i = 0; i < tamanho; ++i) {
+            for (int j = i + 1; j < tamanho; ++j) {
+                ++contadores[entrada.get(i).intervalo_desord(entrada.get(j)) - 1];
             }
         }
-
         return contadores;
     }
 }
