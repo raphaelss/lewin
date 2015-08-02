29a59eea7f85846542a8d0e1d7fe4a79935a61fe
Raphael Santos <contact@raphaelss.com>
2015-08-01 21:54:19 -0300

    Remoção de método main em TipoTabela.

--- src/main/java/GUI/TipoTabela.java
+++ src/main/java/GUI/TipoTabela.java
@@ -24,15 +24,9 @@ import java.io.PrintWriter;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 
-/**
- *
- * @author Hildegard
- */
 public class TipoTabela extends javax.swing.JFrame {
     private JCheckBox[] checkBoxes;
-    /**
-     * Creates new form TipoTabela
-     */
+
     public TipoTabela() {
         initComponents();
 
@@ -135,47 +129,6 @@ public class TipoTabela extends javax.swing.JFrame {
         setVisible(false);
     }//GEN-LAST:event_botaoExibirTabelaActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /*
-         * Set the Nimbus look and feel
-         */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /*
-         * If Nimbus (introduced in Java SE 6) is not available, stay with the
-         * default look and feel. For details see
-         * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TipoTabela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TipoTabela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TipoTabela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TipoTabela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /*
-         * Create and display the form
-         */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-
-            public void run() {
-                new TipoTabela().setVisible(true);
-            }
-        });
-    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton botaoExibirTabela;
     private javax.swing.JCheckBox checkBoxHexacordes;
