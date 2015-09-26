aa6b107d24dad8a08c4877b9d335f6db6e2a339b
Raphael Santos <contact@raphaelss.com>
2015-09-26 10:20:59 -0300

    Extract template content to english.json and add copying files capability to generate.js

--- docs/generate.js
+++ docs/generate.js
@@ -2,7 +2,10 @@ var fs = require('fs'),
     hb = require('handlebars');
 
 var outputDir = "build/"
-var template = hb.compile(fs.readFileSync("template.html").toString());
+var template_file = "template.html";
+var to_copy = ["style.css"];
+
+var template = hb.compile(fs.readFileSync(template_file).toString());
 
 function generate(lang) {
   var context = JSON.parse(fs.readFileSync(lang + '.json').toString());
@@ -14,5 +17,10 @@ try {
 } catch (e) {
   if (e.code !== 'EEXIST')
     throw e;
+}
+
+for (var i = 0; i < to_copy.length; ++i) {
+  fs.createReadStream(to_copy[i])
+    .pipe(fs.createWriteStream(outputDir + to_copy[i]));
 }
 generate('english');
